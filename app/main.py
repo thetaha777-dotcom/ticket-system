15c9cd65e1c017141e443c12de1b3210936acde5
Aissulu <kalpena11@gmail.com>
2026-09-09 19:09:36 +0500

    change structure

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/api-endpoint")
+async def first_api():
+        return {'message': 'Hello World!'}
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/api-endpoint")
+async def first_api():
+        return {'message': 'Hello World!'}
